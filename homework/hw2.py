4dbbd440fe20333d4588bd24d173b528dc9770ac
JokerQA <semchyonok.it@mail.ru>
2022-01-13 21:37:07 +0500

    module 3 hw2.py Done!

--- homework/hw2.py
+++ homework/hw2.py
@@ -197,16 +197,56 @@ print("-------------------------------------------------------------------------
 # Boolean
 
 #  48. Создать переменную b_item_t и присвоить True
+
+b_item_t = True
+
 #  49. Создать переменную b_item_f и присвоить False
+
+b_item_f = False
+
 #  50. Создать переменную b_item_relult_sum и присвоить сумму b_item_t и b_item_f
+
+b_item_relult_sum = b_item_t+b_item_f
+
 #  51. Вывести b_item_relult_sum в консоль.
+
+print("True + False", b_item_relult_sum)
+
 #  52. Создать переменную b_item_relult_subtr и присвоить разницу b_item_t и b_item_f
+
+b_item_relult_subtr = b_item_t - b_item_f
+
 #  53. Вывести b_item_relult_subtr в консоль.
+
+print("Разница:", b_item_relult_subtr)
+
 #  54. Создать переменную b_item_relult_multi и присвоить умножение b_item_t и b_item_f
+
+b_item_relult_multi = b_item_t * b_item_f
+
 #  55. Вывести b_item_relult_multi в консоль.
-#  56. Создать переменную b_item_relult_division и присвоить деление b_item_t и b_item_f
-#  57. Вывести b_item_relult_division в консоль. (Получить ошибку)
+
+print("Умножение:", b_item_relult_multi)
+
+#  56 и 57. Вывести b_item_relult_division в консоль. (Получить ошибку)
+try:
+    b_item_relult_division = b_item_t / b_item_f
+    print(b_item_relult_division)
+except ZeroDivisionError:
+    print("На ноль делить нельзя!!!")
+
 #  58. Создать переменную b_item_1_int и присвоить явное приведение b_item_t к int
+
+b_item_1_int = int(b_item_t)
+
 #  59. Вывести b_item_1_int в консоль.
+
+print("Явное присвоение к int = ", b_item_1_int)
+
 #  60. Создать переменную b_item_2_int и присвоить явное приведение b_item_2 к int
+
+b_item_2_int = int(b_item_f)
+
 #  61. Вывести b_item_2_int в консоль.
+
+print("False к int = ", b_item_2_int)
