2dbde02a0213fc64d8501fba43a8aa40b7940c85
JokerQA <semchyonok.it@mail.ru>
2022-01-24 23:10:54 +0500

    Homework 5. 5 tasks done

--- homework/hw5.py
+++ homework/hw5.py
@@ -30,3 +30,21 @@ for item in range(70):
         lists_odd.append(odd_count)
 
 print('odd_number = ', lists_odd)
+
+# 4.Написать скрипт который из списка целых чисел выведет чётные числа.
+
+lists_even_new = []
+
+for item in range(len(lists)):
+    if lists[item] % 2 == 0:
+        lists_even_new.append(lists[item])
+print('Even range from list 1 =', lists_even_new)
+
+# 5.Написать скрипт который из списка целых чисел выведет нечётные числа.
+
+lists_odd_new = []
+
+for item in range(len(lists)):
+    if not lists[item] % 2 == 0:
+        lists_odd_new.append(lists[item])
+print('Odd range from list 1 =', lists_odd_new)
